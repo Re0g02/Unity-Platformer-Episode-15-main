5fa1151262e2dc281b8d98108ec7d3a74f5cb3b4
Re0g02 <roma.abanovich@mail.ru>
2024-05-09 15:35:23 +0300

    Initial commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        
+        Application.Quit(); //Quits the game (only works in build)
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; //Exits play mode (will only be executed in the editor)
+#endif
+    }
+
+    public void SoundVolume()
+    {
+        SoundManager.instance.ChangeSoundVolume(0.2f);
+    }
+    public void MusicVolume()
+    {
+        SoundManager.instance.ChangeMusicVolume(0.2f);
+    }
+}
